3276296b2726d03e970dedc4977ec43bae07b3a0
Emilio <emriller@espol.edu.ec>
2025-01-20 13:12:20 -0500

    request

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Request
 from passlib.context import CryptContext
 from pydantic import BaseModel
 from typing import List
